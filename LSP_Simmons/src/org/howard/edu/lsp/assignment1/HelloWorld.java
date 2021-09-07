19cc60b1df6bd55cbb4af91e76d65810e3ba2ad2
mjdion <dionsimmons13@gmail.com>
2021-09-07 12:38:50 -0400

    Commit message

---
+++ LSP_Simmons/src/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -0,0 +1,11 @@
+package org.howard.edu.lsp.assignment1;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		// TODO Auto-generated method stub
+
+	}
+
+}
